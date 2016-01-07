12aa54a5f6bec1e689dc8f6c91d19d91ee7f5b55
ConneXNL <contact@ivia-solutions.com>
2016-01-07 22:23:47 +0100

    Create CmfExtraExtension.php

---
+++ src/AppBundle/Twig/CmfExtraExtension.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Twig;
+use Symfony\Cmf\Bundle\CoreBundle\Templating\Helper\CmfHelper;
+use Symfony\Cmf\Bundle\SeoBundle\SeoPresentationInterface;
+
+class CmfExtraExtension extends \Twig_Extension
+{
+
+    /**
+     * @var CmfHelper
+     */
+    private $helper;
+
+    /**
+     * @var string
+     */
+    private $defaultContentPath;
+
+    /**
+     * @var SeoPresentationInterface
+     */
+    protected $seoPresentation;
+
+    public function __construct(CmfHelper $helper, $defaultContentPath = '/cms/content/', SeoPresentationInterface $seoPresentation = null)
+    {
+        $this->helper = $helper;
+        $this->defaultContentPath = $defaultContentPath;
+        $this->seoPresentation = $seoPresentation;
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('cmf_find_current_published_content', array($this, 'findCurrentPublishedCMFContent')),
+        );
+    }
+
+    public function findCurrentPublishedCMFContent($contentNames){
+        if (!is_array($contentNames)) $contentNames = array($contentNames);
+        $match = null;
+
+        foreach ($contentNames as $contentName){
+            $contentPath = substr($contentName, 0, 1) !== '/' ? $this->defaultContentPath."/".$contentName : $contentName;
+            $content = $this->helper->find($contentPath);
+            if ($content && $this->helper->isPublished($content)){
+                $this->seoPresentation->updateSeoPage($content);
+                $match = $content;
+                break;
+            }
+        }
+
+        return $match;
+    }
+
+    public function getName()
+    {
+        return 'cmf_extra';
+    }
+}
